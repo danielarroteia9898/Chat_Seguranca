9cbd9b1bc8bc91907d3a4de69a645a4df59de323
Patricia <2202416@my.ipleiria.pt>
2021-04-11 19:49:48 +0100

    Form_login

---
+++ Chat_Seguranca/Client/login.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace Client
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_Username = new System.Windows.Forms.Label();
+            this.label_password = new System.Windows.Forms.Label();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.textBox_username = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label_Username
+            // 
+            this.label_Username.AutoSize = true;
+            this.label_Username.Location = new System.Drawing.Point(345, 66);
+            this.label_Username.Name = "label_Username";
+            this.label_Username.Size = new System.Drawing.Size(73, 17);
+            this.label_Username.TabIndex = 0;
+            this.label_Username.Text = "Username";
+            // 
+            // label_password
+            // 
+            this.label_password.AutoSize = true;
+            this.label_password.Location = new System.Drawing.Point(345, 175);
+            this.label_password.Name = "label_password";
+            this.label_password.Size = new System.Drawing.Size(69, 17);
+            this.label_password.TabIndex = 2;
+            this.label_password.Text = "Password";
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Location = new System.Drawing.Point(286, 331);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(165, 60);
+            this.btn_submit.TabIndex = 3;
+            this.btn_submit.Text = "Entrar";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            // 
+            // textBox_username
+            // 
+            this.textBox_username.Location = new System.Drawing.Point(301, 101);
+            this.textBox_username.Name = "textBox_username";
+            this.textBox_username.Size = new System.Drawing.Size(161, 22);
+            this.textBox_username.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(286, 214);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(161, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox_username);
+            this.Controls.Add(this.btn_submit);
+            this.Controls.Add(this.label_password);
+            this.Controls.Add(this.label_Username);
+            this.Name = "login";
+            this.Text = "login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_Username;
+        private System.Windows.Forms.Label label_password;
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.TextBox textBox_username;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
